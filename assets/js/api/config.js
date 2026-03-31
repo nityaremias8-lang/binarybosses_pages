682acc4bea0aa14966ec3d0e955bcbd3eca3b774
virginiazhu <virginiaz15525@gmail.com>
2026-03-31 12:32:24 -0700

    po

--- assets/js/api/config.js
+++ assets/js/api/config.js
@@ -6,7 +6,7 @@ export const baseurl = "{{ site.baseurl }}";
 
 export var pythonURI;
 if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
-    pythonURI = "http://localhost:8676";  // Same URI for localhost or 127.0.0.1
+    pythonURI = "http://localhost:8376";  // Same URI for localhost or 127.0.0.1
 } else {
     pythonURI = "https://flask.opencodingsociety.com";
 
