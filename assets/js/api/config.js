47fe4bd696731b7359cb730a00148305ad6f6652
virginiazhu <virginiaz15525@gmail.com>
2026-04-01 12:38:14 -0700

    front end

--- assets/js/api/config.js
+++ assets/js/api/config.js
@@ -8,7 +8,7 @@ export var pythonURI;
 if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
     pythonURI = "http://localhost:8376";  // Same URI for localhost or 127.0.0.1
 } else {
-    pythonURI = "https://flask.opencodingsociety.com";
+    pythonURI = "https://fops.opencodingsociety.com";
 
 }
 
